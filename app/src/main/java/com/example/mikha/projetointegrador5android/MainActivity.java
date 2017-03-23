eeacfcfa98c8b498c131cb790fc896045b24f493
Mikhael <mikhael.pc@hotmail.com>
2017-03-23 16:35:40 -0300

    Funcionalidade de text-to-speech implementada.

--- app/src/main/java/com/example/mikha/projetointegrador5android/MainActivity.java
+++ app/src/main/java/com/example/mikha/projetointegrador5android/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.mikha.projetointegrador5android;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech speech;
+    EditText ed1;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1 = (EditText) findViewById(R.id.EditTextEntrada);
+        b1 = (Button) findViewById(R.id.ButtonTextToSpeech);
+        final Locale localeBR = new Locale("pt","BR");
+
+        speech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    speech.setLanguage(localeBR);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                speech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(speech != null){
+            speech.stop();
+            speech.shutdown();
+        }
+        super.onPause();
     }
 }
