ffb9654640f9762a057618ffe17e6f8081c252c7
Mikhael <mikhael.pc@hotmail.com>
2017-05-31 14:14:38 -0300

    Tela inicial virou o Login.

--- app/src/main/java/com/example/mikha/projetointegrador5android/LoginActivity.java
+++ app/src/main/java/com/example/mikha/projetointegrador5android/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.mikha.projetointegrador5android;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,7 +44,8 @@ public class LoginActivity extends AppCompatActivity {
                             Log.w("tag", "signInWithEmail:failed", task.getException());
                             Toast.makeText(LoginActivity.this, "falhou", Toast.LENGTH_SHORT).show();
                         }else{
-                            finish();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
                         }
                     }
                 });
